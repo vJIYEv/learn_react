ab9cb6367fc213bbc8c40518405d32c19c79f5a5
vJIYEv <jiye.kim@lge.com>
2023-07-03 18:26:05 +0900

    230703

---
+++ run.dev_learn/11-1.Implement_a_traffic_light.js
@@ -0,0 +1,25 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
+
+  function handleClick() {
+    setWalk(!walk);
+    alert((walk? 'Stop' : 'Walk') + ' is next');
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        Change to {walk ? 'Stop' : 'Walk'}
+      </button>
+      <h1 style={{
+        color: walk ? 'darkgreen' : 'darkred'
+      }}>
+        {walk ? 'Walk' : 'Stop'}
+      </h1>
+    </>
+  );
+}
---
+++ run.dev_learn/12-2.Implement_the_state_queue_yourself.js
@@ -0,0 +1,91 @@
+'use client';
+
+import { useState } from 'react';
+
+function getFinalState(baseState, queue) {
+  let finalState = baseState;
+  
+  // TODO: do something with the queue...
+  for (let i=0; i<queue.length; i++) {
+    if (typeof(queue[i]) === 'number') {
+      finalState = queue[i];
+    } else {
+      let func = queue[i];
+      finalState = func(finalState);  
+    }
+  }
+  return finalState;
+}
+
+function increment(n) {
+  return n + 1;
+}
+increment.toString = () => 'n => n+1';
+
+export default function App() {
+  return (
+    <>
+      <TestCase
+        baseState={0}
+        queue={[1, 1, 1]}
+        expected={1}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          increment,
+          increment,
+          increment
+        ]}
+        expected={3}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          5,
+          increment,
+        ]}
+        expected={6}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          5,
+          increment,
+          42,
+        ]}
+        expected={42}
+      />
+    </>
+  );
+}
+
+function TestCase({
+  baseState,
+  queue,
+  expected
+}) {
+  const actual = getFinalState(baseState, queue);
+  return (
+    <>
+      <p>Base state: <b>{baseState}</b></p>
+      <p>Queue: <b>[{queue.join(', ')}]</b></p>
+      <p>Expected result: <b>{expected}</b></p>
+      <p style={{
+        color: actual === expected ?
+          'green' :
+          'red'
+      }}>
+        Your result: <b>{actual}</b>
+        {' '}
+        ({actual === expected ?
+          'correct' :
+          'wrong'
+        })
+      </p>
+    </>
+  );
+}
