6db032f47c1b1f0e671eed7d33f85d37cd59efa7
vJIYEv <jiye.kim@lge.com>
2023-07-17 16:21:36 +0900

    230717

---
+++ run.dev_learn/18-1.Fix_disappearing_input_text.js
@@ -0,0 +1,28 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function App() {
+  const [showHint, setShowHint] = useState(false);
+  return (
+    <div>
+      {showHint && 
+        <p><i>Hint: Your favorite city?</i></p>
+      }
+      <Form />
+        <button onClick={() => {
+          setShowHint(!showHint);
+        }}>{showHint ? 'Hide' : 'Show'} hint</button>
+    </div>
+  );
+}
+
+function Form() {
+  const [text, setText] = useState('');
+  return (
+    <textarea
+      value={text}
+      onChange={e => setText(e.target.value)}
+    />
+  );
+}
---
+++ run.dev_learn/18-5.Fix_misplaced_state_in_the_list.js
@@ -0,0 +1,57 @@
+'use client';
+
+import { useState } from 'react';
+
+function Contact({ contact }) {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <>
+      <p><b>{contact.name}</b></p>
+      {expanded &&
+        <p><i>{contact.email}</i></p>
+      }
+      <button onClick={() => {
+        setExpanded(!expanded);
+      }}>
+        {expanded ? 'Hide' : 'Show'} email
+      </button>
+    </>
+  );
+}
+
+export default function ContactList() {
+  const [reverse, setReverse] = useState(false);
+
+  const displayedContacts = [...contacts];
+  if (reverse) {
+    displayedContacts.reverse();
+  }
+
+  return (
+    <>
+      <label>
+        <input
+          type="checkbox"
+          value={reverse}
+          onChange={e => {
+            setReverse(e.target.checked)
+          }}
+        />{' '}
+        Show in reverse order
+      </label>
+      <ul>
+        {displayedContacts.map((contact) =>
+          <li key={contact.name}>
+            <Contact contact={contact} />
+          </li>
+        )}
+      </ul>
+    </>
+  );
+}
+
+const contacts = [
+  { id: 0, name: 'Alice', email: 'alice@mail.com' },
+  { id: 1, name: 'Bob', email: 'bob@mail.com' },
+  { id: 2, name: 'Taylor', email: 'taylor@mail.com' }
+];
